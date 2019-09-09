da1f69a4da0dfa5a3548d0ffe1d83871fb9f128a
Godmar Back <godmar@gmail.com>
2019-09-09 09:58:08 -0400

    trying to reproduce MDX parsing bug

---
+++ src/components/Notice.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import styled from 'styled-components'
+
+const HistoryBlock = styled.blockquote`
+  padding: 10px 30px 10px 30px;
+  margin: 30px 0;
+  border-radius: 5px;
+  background: lightgreen;
+  color: black;
+  p {
+    margin: 5px 0 10px;
+  }
+`
+
+export default (props) => {
+    const {type, title, children} = props;
+    const jsx = children
+    console.dir(jsx)
+    if (type === "tip") {
+        return (<HistoryBlock>
+                <div>
+                    <em>{title}</em>
+                </div>
+                {jsx}
+            </HistoryBlock>);
+    } else {
+        return jsx;
+    }
+}
